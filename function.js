2ec5ceb631a2316b7572f567a25fd1f7147905e4
Zarif <Zaripzzz302@gmail.com>
2025-04-14 16:55:52 +0800

    Update function.js

--- function.js
+++ function.js
@@ -43,3 +43,44 @@ function orderFood() {
     priceInfo.innerHTML = "<strong>Total Price : RM " + price + "\n </strong>";
 }
 
+let btnKlik = document.getElementById('klik');
+
+btnKlik.onclick = function() {
+    alert('Button ditekan');
+}
+
+let txtWelcome = document.getElementById('welcome');
+
+txtWelcome.onmouseover = function() {
+    txtWelcome.innerText = "Selamat Datang";
+}
+
+txtWelcome.onmouseout = function() {
+    txtWelcome.innerText = "Welcome";
+}
+
+let cow = document.getElementById('cow');
+
+cow.onmouseover = function() {
+    this.src = "https://bythehorns.co.nz/cdn/shop/products/whitehighlandcowcolourweb_d4db7dec-4646-4fb1-955e-668d6290f274.jpg?v=1604216394&width=1080";
+
+    this.style.filter = "blur(0px)";
+    this.style.transform = "scale(2)";
+    this.style.transition = "transform 1.0s ease-in-out";
+}
+
+cow.onmouseout = function() {
+    this.src = "https://www.betterwithdairy.com/sites/default/files/2025-02/What_Does_Dairy_Cow_Eat.webp";
+
+    this.style.filter = "blur(2px)";
+    this.style.transform = "scale(1)";
+}
+
+function checkJabatan() {
+    let jabatan = document.getElementById('jabatan');
+    let selectedJabatan = jabatan.options[jabatan.selectedIndex].value;
+
+    alert("You have selected : " + selectedJabatan);
+    console.log(selectedJabatan);
+}
+
